15ae65b5018760af7162636c395639f9ea407348
Christopher Su <christophersu9@gmail.com>
2015-05-17 19:35:57 -0700

    more debug items

--- README.md
+++ README.md
@@ -9,5 +9,7 @@ Python API for getting prices from the Steam market.
 import steam_market as sm
 from steam_market import encode_for_url
 
-item = sm.get_tf2_item(encode_for_url('Strange Killstreak Flame Thrower'))
+item = sm.get_tf2_item(encode_for_url('Strange Professional Killstreak Scattergun'))
+for listing in item.listings:
+    print listing.price
 ```
--- debug.py
+++ debug.py
@@ -1,6 +1,20 @@
 import steam_market as sm
 from steam_market import encode_for_url
 
-item = sm.get_tf2_item(encode_for_url('Strange Killstreak Flame Thrower'))
-for listing in item.listings:
-    print listing.price
+items = [
+    'Strange Professional Killstreak Minigun',
+    'Strange Professional Killstreak Mantreads',
+    'Strange Professional Killstreak Overdose',
+    'Strange Professional Killstreak Winger',
+    'Strange Professional Killstreak Degreaser',
+    'Strange Professional Killstreak Scattergun',
+    'Strange Professional Killstreak Wrench',
+    'Strange Professional Killstreak Knife',
+    'Strange Professional Killstreak Rainblower',
+    'Strange Professional Killstreak Huntsman'
+]
+
+for item in items:
+    print item
+    market_item = sm.get_tf2_item(encode_for_url(item))
+    print [i.price for i in market_item.listings]
