d5d58d9dc1b27cc083cf730039208ab722199d43
Christopher Su <christophersu9@gmail.com>
2015-05-23 15:47:49 -0700

    pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+  name = 'steam_market',
+  packages = ['steam_market'],
+  version = '0.0.1',
+  description = 'A Python API for getting prices from the Steam market.',
+  author = 'Christopher Su',
+  author_email = 'chris+gh@christopher.su',
+  url = 'https://github.com/csu/PySteamMarket',
+  download_url = 'https://github.com/csu/PySteamMarket/archive/master.zip',
+)
