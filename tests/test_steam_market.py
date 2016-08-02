9550c015027c0715d60cd354136dff31bcc7fb9f
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2016-08-01 21:56:41 -0700

    Diversify list of items used for testing

--- tests/test_steam_market.py
+++ tests/test_steam_market.py
@@ -5,20 +5,27 @@ import steam_market as sm
 
 class TestTF2Items(unittest.TestCase):
     def runTest(self):
-        items = [
+        tf2_items = [
             'Strange Professional Killstreak Minigun',
-            'Strange Professional Killstreak Mantreads',
-            'Strange Professional Killstreak Overdose',
-            'Strange Professional Killstreak Winger',
-            'Strange Professional Killstreak Degreaser',
-            'Strange Professional Killstreak Scattergun',
-            'Strange Professional Killstreak Wrench',
-            'Strange Professional Killstreak Knife',
-            'Strange Professional Killstreak Rainblower',
-            'Strange Professional Killstreak Huntsman'
+            'Vintage Gunboats',
+            'Name Tag',
+            'Mann Co. Supply Crate Key'
         ]
 
-        for item in items:
+        csgo_items = [
+            'Gamma Case',
+            '\u2605 Karambit | Bright Water (Factory New)',
+            'AK-47 | Frontside Misty (Field-Tested)'
+        ]
+
+        print('Testing TF2 Items:\n')
+        for item in tf2_items:
             print(item)
             market_item = sm.get_tf2_item(item)
+            print([i.price for i in market_item.listings])
+
+        print('\nTesting CS:GO Items:\n')
+        for item in csgo_items:
+            print(item)
+            market_item = sm.get_csgo_item(item)
             print([i.price for i in market_item.listings])
